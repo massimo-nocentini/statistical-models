82ea1906636d57f19daf0f75e31f5d0dc3cdb129
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-10-21 19:42:51 +0200

    Fixed the exercise 2.7 and added to it a significance test.

--- r-sources/exercises/chapter-two/two-seven.R
+++ r-sources/exercises/chapter-two/two-seven.R
@@ -1,17 +1,20 @@
 
 twoSeven <- function(){
 
+  nullHp <- 25
   dimension <- 30
   level <- .05
   empiricalMean <- 26.3
-  standardError <- 2.4/sqrt(30)
-  tOss <- qt(df=dimension-1, p=level/2, lower.tail=FALSE)
+  standardError <- 2.4/sqrt(dimension)
+  superiorQuart <- qt(df=dimension-1, p=level/2, lower.tail=FALSE)
+  tOss <- (empiricalMean - nullHp)/standardError
 
-  return(list(
+  return(list(           
            tOss=tOss,
-           lower=empiricalMean - tOss*standardError,
-           upper=empiricalMean + tOss*standardError))
-  
+           confidenceInterval= c(empiricalMean - superiorQuart*standardError,
+             empiricalMean + superiorQuart*standardError),
+           pValue=pt(df=dimension-1, lower.tail=FALSE, q=tOss)
+           ))    
 }
 
 
