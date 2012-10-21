111fd5f41121078cb5fdf9da781f362270db6f52
Massimo Nocentini <massimo.nocentini@gmail.com>
2012-10-21 17:33:44 +0200

    Cleaned up some git-repository mess.

--- README.md
+++
@@ -1,4 +0,0 @@
-statistical-models
-==================
-
-Content of the course Statistical Models given by Giovanni Marchetti at University of Florence
---
+++ r-sources/exercises/chapter-two/two-six.R
@@ -0,0 +1,33 @@
+twosix <- function(){
+  dimension <- 10000
+  uniformVector <- rep(0, dimension)
+  for (i in 1:dimension) {
+    sample <- runif(n=20, min=0, max=100)
+    uniformVector[i] <- 2*mean(sample)
+  }
+    
+  results <- list(estimatorVector=uniformVector,
+              empiricalMean=mean(uniformVector),
+              empiricalVar=var(uniformVector),
+              empiricalVarComputedByHand=sum((
+                uniformVector-mean(uniformVector))^2)/(dimension-1))
+
+  
+  postscript("two-six.ps", horizontal = FALSE)  # set graphical output file
+  hist(twoSixResults$estimatorVector,
+       freq=FALSE,
+       breaks=20,
+       xlab = expression(T=2*bar(Y)),
+       ylim=c(0, .04))
+  lines(density(twoSixResults$estimatorVector), lty=2, col="blue")
+  curve(dnorm(x, 
+              mean = twoSixResults$empiricalMean, 
+              sd = sqrt(twoSixResults$empiricalVar)), 
+        from=50, 
+        to=160, 
+        add=TRUE, 
+        col="red")
+  dev.off()  # close the graphical output file
+    
+  return(results)
+}
